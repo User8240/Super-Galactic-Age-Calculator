d74ba2927a805e430cfb40c33e0c7b7f1fc5b41d
Grace Kostanich <fl5812597@gmail.com>
2022-07-04 14:34:37 -0700

    Add passing GREEN tests | correctly return *any planet* age and year(s) lived over life expectancy message

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -6,20 +6,41 @@ describe('Calc', () => {
     expect(calcObject.age).toEqual(30);
     expect(calcObject.lifeExpect).toEqual(100);
   });
-  test('should correctly return Mercury age, years left, and corresponding life expectancy message', () => {
-    const calcObject = new Calc(30,100);
-    expect(calcObject.CalcMercury()).toEqual("You are 125 years old on Mercury. You have 292 years left to live!");
+  test('should correctly create a calcObject object holding age and life expectancy', () => {
+    const calcObject = new Calc(90,80);
+    expect(calcObject.age).toEqual(90);
+    expect(calcObject.lifeExpect).toEqual(80);
   });
-  test('should correctly return Venus age, years left, and corresponding life expectancy message', () => {
+  test('should correctly return Mercury age, year(s) left, and corresponding life expectancy message', () => {
     const calcObject = new Calc(30,100);
-    expect(calcObject.CalcVenus()).toEqual("You are 48 years old on Venus. You have 113 years left to live!");
+    expect(calcObject.CalcMercury()).toEqual("You are 125 year(s) old on Mercury. You have 292 year(s) left to live!");
   });
-  test('should correctly return Mars age, years left, and corresponding life expectancy message', () => {
-    const calcObject = new Calc(30,100);
-    expect(calcObject.CalcMars()).toEqual("You are 16 years old on Mars. You have 37 years left to live!");
+  test('should correctly return Mercury age and year(s) lived over life expectancy message', () => {
+    const calcObject = new Calc(90,80);
+    expect(calcObject.CalcMercury()).toEqual("You are 375 year(s) old on Mercury. You are 42 year(s) over your life expectancy!");
   });
-  test('should correctly return Jupiter age, years left, and corresponding life expectancy message', () => {
+  test('should correctly return Venus age, year(s) left, and corresponding life expectancy message', () => {
     const calcObject = new Calc(30,100);
-    expect(calcObject.CalcJupiter()).toEqual("You are 3 years old on Jupiter. You have 6 years left to live!");
+    expect(calcObject.CalcVenus()).toEqual("You are 48 year(s) old on Venus. You have 113 year(s) left to live!");
+  });
+  test('should correctly return Venus age and year(s) lived over life expectancy message', () => {
+    const calcObject = new Calc(90,80);
+    expect(calcObject.CalcVenus()).toEqual("You are 145 year(s) old on Venus. You are 16 year(s) over your life expectancy!");
+  });
+  test('should correctly return Mars age, year(s) left, and corresponding life expectancy message', () => {
+    const calcObject = new Calc(30,100);
+    expect(calcObject.CalcMars()).toEqual("You are 16 year(s) old on Mars. You have 37 year(s) left to live!");
+  });
+  test('should correctly return Mars age and year(s) lived over life expectancy message', () => {
+    const calcObject = new Calc(90,80);
+    expect(calcObject.CalcMars()).toEqual("You are 48 year(s) old on Mars. You are 5 year(s) over your life expectancy!");
+  });
+  test('should correctly return Jupiter age, year(s) left, and corresponding life expectancy message', () => {
+    const calcObject = new Calc(30,100);
+    expect(calcObject.CalcJupiter()).toEqual("You are 3 year(s) old on Jupiter. You have 6 year(s) left to live!");
+  });
+  test('should correctly return Jupiter age and year(s) lived over life expectancy message', () => {
+    const calcObject = new Calc(90,80);
+    expect(calcObject.CalcJupiter()).toEqual("You are 8 year(s) old on Jupiter. You are 1 year(s) over your life expectancy!");
   });
 });
